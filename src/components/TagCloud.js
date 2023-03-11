f77e1842b195938cc5a8b4a52c2b8d955a5420ba
jooyoung <jooyoung@pood.pet>
2023-03-11 12:05:35 +0900

    add: tagcloud custom

--- src/components/TagCloud.js
+++ src/components/TagCloud.js
@@ -2,6 +2,14 @@ import React from "react";
 import { TagCloud } from "react-tagcloud";
 
 function tagCloud() {
+  const bgImageRandom = [
+    "https://blog.kakaocdn.net/dn/cBOYPU/btqSXjlLpaY/sNnDtHL2Hsfw7JHgkTRIjk/img.jpg",
+    "https://blog.kakaocdn.net/dn/b80sq3/btqDli9NMy6/ZqnKTEIgpYxVfNyEkejZjK/img.png",
+    "https://cdn.shopify.com/s/files/1/0575/4532/6644/products/KakaoTalk_20220827_153301760_02_740x.jpg?v=1661590423",
+    "https://cdn.shopify.com/s/files/1/0575/4532/6644/products/KakaoTalk_20220712_131510124_02_740x.jpg?v=1657771184"
+  ];
+  //   const randomValue =
+  //     bgImageRandom[Math.floor(Math.random() * bgImageRandom.length)];
   const customRenderer = (tag, size, color) => (
     <span
       key={tag.value}
@@ -11,11 +19,11 @@ function tagCloud() {
         padding: "3px",
         display: "inline-block",
         color: "white",
-        // backgroundImage: URL(
-        //   "https://blog.kakaocdn.net/dn/cBOYPU/btqSXjlLpaY/sNnDtHL2Hsfw7JHgkTRIjk/img.jpg"
-        // ),
         backgroundImage:
           "url('https://blog.kakaocdn.net/dn/cBOYPU/btqSXjlLpaY/sNnDtHL2Hsfw7JHgkTRIjk/img.jpg')",
+        // backgroundImage: `url(${randomValue})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
         width: "200px",
         height: "200px"
         // background: `${color}`
@@ -26,13 +34,43 @@ function tagCloud() {
   );
 
   const data = [
-    { value: "jQuery", count: 15 },
-    { value: "MongoDB", count: 18 },
-    { value: "JavaScript", count: 28 },
-    { value: "React", count: 20 },
-    { value: "Nodejs", count: 28 },
-    { value: "Express.js", count: 25 }
+    {
+      value: "dsfsdfsdfsdfsdfs",
+      count: 15,
+      props: {
+        style: {
+          color: "red",
+          backgroundImage:
+            "url(https://cdn.shopify.com/s/files/1/0575/4532/6644/products/KakaoTalk_20220712_131510124_02_740x.jpg?v=1657771184)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          width: "200px",
+          height: "200px",
+          padding: "20px"
+        }
+      }
+    },
+    {
+      value: "hello",
+      count: 18,
+      props: {
+        style: {
+          color: "red",
+          backgroundImage:
+            "url(https://blog.kakaocdn.net/dn/cBOYPU/btqSXjlLpaY/sNnDtHL2Hsfw7JHgkTRIjk/img.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "contain",
+          width: "200px",
+          height: "200px",
+          padding: "20px"
+        }
+      }
+    },
+    { value: "hi", count: 28 },
+    { value: "hellllllloooooooo", count: 20 },
+    { value: "sdfsdfsdfsdfsdfsdf", count: 28 }
   ];
+  console.log(data[0].props.style.backgroundImage);
   return (
     <div style={{ border: "1px solid blue" }}>
       <TagCloud
@@ -41,7 +79,7 @@ function tagCloud() {
         tags={data}
         // style={{ width: 300 }}
         className="myTagCloud"
-        renderer={customRenderer}
+        // renderer={customRenderer}
       />
     </div>
   );
