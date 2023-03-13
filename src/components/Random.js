5380034d6d23e64e9a64c70e4f01732cb34a7d17
jooyoung <jooyoung@pood.pet>
2023-03-13 21:55:47 +0900

    add: random letters

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,21 +1,71 @@
 import "./Random.css";
 
 const MyComponent = () => {
+  //   const bgImageArr = [
+  //     "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test1.png?raw=true",
+  //     "https://cdn.shopify.com/s/files/1/0575/4532/6644/products/KakaoTalk_20220827_153301760_02_740x.jpg?v=1661590423",
+  //     "https://blog.kakaocdn.net/dn/b80sq3/btqDli9NMy6/ZqnKTEIgpYxVfNyEkejZjK/img.png"
+  //   ];
   const arr = [
     {
-      text: "좋아좋아 하모니카 솔로 ~~",
+      text: "안녕 안녕 반가워 안녕 안녕 반가워",
       idx: 0,
-      url: "https://blog.kakaocdn.net/dn/cBOYPU/btqSXjlLpaY/sNnDtHL2Hsfw7JHgkTRIjk/img.jpg"
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test2.png?raw=true"
     },
     {
-      text: "꽃송이가 꽃송이가 꽃송이가 그래그래 피었네",
-      idx: 0,
-      url: "https://cdn.shopify.com/s/files/1/0575/4532/6644/products/KakaoTalk_20220827_153301760_02_740x.jpg?v=1661590423"
+      text: "반갑습니다 222222",
+      idx: 1,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test1.png?raw=true"
     },
     {
-      text: "거리에 사람들 ~ 수많은 사람들 ~ 그래 나는 네게 얼마만큼 특별한건지 그게 어려운거야 ~",
-      idx: 0,
-      url: "https://blog.kakaocdn.net/dn/b80sq3/btqDli9NMy6/ZqnKTEIgpYxVfNyEkejZjK/img.png"
+      text: "안녕하세요 안녕하세요",
+      idx: 2,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test3.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까 ?",
+      idx: 3,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까2 ?",
+      idx: 4,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까3 ?",
+      idx: 5,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까4 ?",
+      idx: 6,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까5 ?",
+      idx: 7,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까6 ?",
+      idx: 8,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까7 ?",
+      idx: 9,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까8 ?",
+      idx: 10,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
+    },
+    {
+      text: "안녕하세요 안녕하시렵니까9 ?",
+      idx: 11,
+      url: "https://github.com/LetterRabbit/front/blob/feature/editor/public/images/test4.png?raw=true"
     }
   ];
   const styledRandom = (idx) => {
@@ -36,11 +86,22 @@ const MyComponent = () => {
         "px) "
     };
   };
+  const bgImage = (idx) => {
+    // const randomNumber = Math.floor(Math.random() * bgImageArr.length);
+    const randomBg = arr[idx];
+    return randomBg.url;
+  };
+
   return (
-    <div>
+    <div className="letters-wrap">
       {arr.map((item, idx) => {
         return (
-          <div key={idx} style={styledRandom(idx)} className="random">
+          <div
+            key={idx}
+            style={styledRandom(idx)}
+            data-content={bgImage(idx)}
+            className="random"
+          >
             {item.text}
           </div>
         );
