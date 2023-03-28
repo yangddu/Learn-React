57bcbb35ad5f6dc9bd59953cea5d52d4e67f05fe
jooyoung <jooyoung@pood.pet>
2023-03-28 10:20:08 +0900

    add: Coins

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,51 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface Params {
   coinId: string;
 }
 
+interface RouteState {
+  name: string;
+}
 const Coin = () => {
-  const { coinId } = useParams<Params>();
-  return <h1>Coin : {coinId}</h1>;
+  //   const { coinId } = useParams<Params>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인 {state?.name || "Loading..."} </Title>
+      </Header>
+      {loading ? <Loader> Loading ...</Loader> : null}
+    </Container>
+  );
 };
 export default Coin;
+
+const Title = styled.h1`
+  font-size: 48px;
+
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Container = styled.div`
+  padding: 50px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+  padding: 30px 0;
+  font-weight: 700;
+  font-size: 30px;
+`;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 const coins = [
   {
@@ -31,19 +41,44 @@ const coins = [
     type: "token"
   }
 ];
+
 const Coins = () => {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 30));
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name }
+                }}
+              >
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 };
@@ -55,7 +90,7 @@ const Title = styled.h1`
 `;
 
 const Container = styled.div`
-  padding: 0 20px;
+  padding: 50px 20px;
 `;
 
 const Header = styled.header`
@@ -63,6 +98,7 @@ const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 40px;
 `;
 
 const CoinsList = styled.ul``;
@@ -74,13 +110,34 @@ const Coin = styled.li`
   margin-bottom: 10px;
   border-radius: 15px;
   a {
+    display: flex;
+
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
       color: ${(props) => props.theme.accentColor};
     }
   }
+`;
+
+const CoinWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+  padding: 30px 0;
+  font-weight: 700;
+  font-size: 30px;
+`;
+
+const Img = styled.img`
+  width: 30px;
+  margin-right: 10px;
 `;
